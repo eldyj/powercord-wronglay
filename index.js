0f523af9a5adcead7201386242565dd03e025a9d
Eldyj <103959122+Eldyj@users.noreply.github.com>
2022-07-29 19:04:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const { Plugin } = require("powercord/entities");
 
-const ru = "йцукенгшщзхъ\\фывапролджэячсмитьбю.\\1234567890ёЁ!\"№;%:?*()-=_+,"
-     eng = "qwertyuiop[]\\asdfghjkl;'zxcvbnm,./1234567890`~!@#$%^&*()-=_+"
+const ru = " йцукенгшщзхъфывапролджэячсмитьбю.ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ.1234567890ёЁ!\"№;%:?*()-=_+";
+     eng = " qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>?1234567890`~!@#$%^&*()-=_+";
 
 function wl_decode(string){
 	let is_rus = false;
 	for(let i = 0; i < ru.length; i++){
-		is_rus = is_rus || string[0] == ru[i]
+		is_rus = is_rus || string[0] == ru[i];
 	}
-	let cur = is_rus ? ru : eng
-	let second = is_rus ? eng : ru
 	
-	for(let i = 0; i < string.length){
-		for(let j = 0 j < ru.length; j++){
-			string[i] == cur[j] ? string[i] = second[j]
+	let cur = is_rus ? ru : eng;
+	let second = is_rus ? eng : ru;
+	let result = ''
+	for(let i = 0; i < string.length; i++){
+		for(let j = 0; j < cur.length; j++){
+			if(string[i] == cur[j]){
+				result += second[j];
+			}
 		}
 	}
-	return string
+	return result;
 }
 
-module.exports = class WrongLay extends Plugin {
+module.exports = class Wronglay extends Plugin {
   async startPlugin() {
     powercord.api.commands.registerCommand({
-      command: "wl",
+      command: "wrong_lay",
       description: "decode wrong layout",
       usage: "{c} [ text ]",
-      executor: args => {
+      executor: (args) => {
         return {
             send: true,
             result: wl_decode(args.join(" "));
         }
       }
-    });
+    })
   }
-};
+}
